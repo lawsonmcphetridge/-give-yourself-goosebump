80fbd0d75022313a1ec2bcea2420429bcd35d6e9
Lawson <lawsonmcphetridge2020@gmail.com>
2022-12-14 09:47:58 -0800

    ci

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { useGoosebump } from '../../hooks/useGoosebump';
-import { useParams } from 'react-router-dom';
 
 export default function Overview({ id, page_id, target_page, option_text }) {
   return (
